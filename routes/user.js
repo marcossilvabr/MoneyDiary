96dc710b25562c308e0ddb792eaec900875baddf
kellybarber <kelly.james.barber@gmail.com>
2017-08-11 21:33:36 +0000

    working registration adding user to database

--- routes/user.js
+++ routes/user.js
@@ -11,13 +11,25 @@ module.exports = (db, User) => {
   })
 
   router.post("/register", (req, res) => {
-    let firstName = req.body.firstName
-    let lastName = req.body.lastName
-    let email = req.body.email
-    let password = bcrypt.hashSync(req.body.password, 10)
+    // let firstName = req.body.firstName
+    // let lastName = req.body.lastName
+    // let email = req.body.email
+    // let password = bcrypt.hashSync(req.body.password, 10)
     // let hashedPassword = bcrypt.hashSync(password, 10);
 
-    
+    let user = new User
+    user.firstName = req.body.firstName
+    user.lastName = req.body.lastName
+    user.email = req.body.email
+    user.password = bcrypt.hashSync(req.body.password, 10)
+
+    user.save((err) => {
+      if (err)
+        res.send(err);
+
+      
+      res.redirect('./register');
+    });
 
   })
 
