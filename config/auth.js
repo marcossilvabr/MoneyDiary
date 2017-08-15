7123d7f7e1c7dfd181e43b0d81d29616329fc8c7
kellybarber <kelly.james.barber@gmail.com>
2017-08-15 19:49:04 +0000

    working in facebook authentication

---
+++ config/auth.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '280457585768862',
+        'clientSecret'  : 'f6b3727e2ed195d62b27d10b9b47a677', 
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
