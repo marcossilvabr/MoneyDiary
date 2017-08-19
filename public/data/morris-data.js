3592669ae01297a71c86ae5ca8b32653f5aeec30
Marcos Silva <marcosgustavobh@gmail.com>
2017-08-19 15:56:07 +0000

    graph bug fixed

--- public/data/morris-data.js
+++ public/data/morris-data.js
@@ -1,136 +1,165 @@
 $(document).ready(function() {
-  function call() {
-    $.ajax({
-    type: "GET",
-    url: "/cashflowData",
-       success: function(event) {
-          result = event;
+   function call() {
+      $.ajax({
+         type: "GET",
+         url: "/cashflowData",
+         success: function(event) {
+            result = event;
 
-          const data = result['data']
+            const data = result['data']
 
-          function getTotalByMonth(data) {
-            // Sets the current month to the first month available
-            let currentMonth = data[0]['date'].split('/', 2)[1]
-            let runningTotal = 0
-            let monthlyTotal = []
+            function getTotalByMonth(data) {
 
-            data.forEach(( element, index ) => {
+               // Sets the current month to the first month available
+               let currentMonth = data[0]['date'].split('/', 2)[1]
+               let runningTotal = 0
+               let monthlyTotal = []
 
-              let month = element['date'].split('/', 2)[1]
+               data.forEach(( element, index ) => {
 
-              // If the element is not the last one in the array:
+                  let month = element['date'].split('/', 2)[1]
 
-                // If the current element has the same month as the last element:
-                if ( month == currentMonth ) {
-                  runningTotal += Number(element['amount'])
+                  // If the element is not the last one in the array:
 
-                  // If the element is the last one in the data array
-                  if ( index == data.length-1 ) {
-                    let monthData = { date   : currentMonth,
-                                      amount : runningTotal }
+                  // If the current element has the same month as the last element:
+                  if ( month == currentMonth ) {
+                     runningTotal += Number(element['amount'])
 
-                    monthlyTotal.push(monthData)
+                     // If the element is the last one in the data array
+                     if ( index == data.length-1 ) {
+                        let monthData = { date   : currentMonth,
+                                           amount : runningTotal }
+
+                        monthlyTotal.push(monthData)
+                     }
+
+                  // If the element has a new month from the last one:
+                  } else {
+                     let monthData = { date   : currentMonth,
+                                       amount : runningTotal }
+
+                     monthlyTotal.push(monthData)
+
+                     runningTotal = 0
+                     runningTotal += Number(element['amount'])
+                     currentMonth = month
+
+                     // If the element is the last one in the data array
+                        if ( index == data.length-1 ) {
+                           let finalData = { date   : currentMonth,
+                                             amount : runningTotal }
+
+                           monthlyTotal.push(finalData)
+                        }
+                  }
+               })
+
+               function reorderList(monthlyTotal) {
+
+                  const totalByMonth = monthlyTotal
+                  let monthObject    = {}
+                  let newMonthlyTotal = []
+
+                  totalByMonth.forEach(( element ) => {
+
+                     if ( !monthObject[element['date']] ) {
+                        monthObject[element['date']] = element['amount']
+                     } else {
+                        monthObject[element['date']] += element['amount']
+                     }
+
+                  })
+
+                  for ( month in monthObject ) {
+
+                  let monthlyData = { date   : month,
+                                      amount : monthObject[month] }
+
+                  newMonthlyTotal.push(monthlyData)
+
                   }
 
-                // If the element has a new month from the last one:
-                } else {
-                  let monthData = { date   : currentMonth,
-                                    amount : runningTotal }
 
-                  monthlyTotal.push(monthData)
+                  // function graph(newMonthlyTotal) {
+                     var Jan = Number()
+                     var Feb = Number()
+                     var Mar = Number()
+                     var Apr = Number()
+                     var May = Number()
+                     var Jun = Number()
+                     var Jul = Number()
+                     var Aug = Number()
+                     var Sep = Number()
+                     var Oct = Number()
+                     var Nov = Number()
+                     var Dec = Number()
 
-                  runningTotal = 0
-                  runningTotal += Number(element['amount'])
-                  currentMonth = month
+                     month = newMonthlyTotal
+                     console.log(newMonthlyTotal)
 
-                  // If the element is the last one in the data array
-                  if ( index == data.length-1 ) {
-                    let finalData = { date   : currentMonth,
-                                      amount : runningTotal }
+                     for (var i = 0; i <= 12; i++) {
+                        if (month[i] === undefined) {
+                        i++
+                        } else if (month[i].date == 01) {
+                           Jan = month[i].amount
+                        } else if (month[i].date == 02) {
+                           Feb = month[i].amount
+                        } else if (month[i].date == 03) {
+                           Mar = month[i].amount
+                        } else if (month[i].date == 04) {
+                           Apr = month[i].amount
+                        } else if (month[i].date == 05) {
+                           May = month[i].amount
+                        } else if (month[i].date == 06) {
+                           Jun = month[i].amount
+                        } else if (month[i].date == 07) {
+                           Jul = month[i].amount
+                        } else if (month[i].date == 08) {
+                           Aug = month[i].amount
+                        } else if (month[i].date == 09) {
+                           Sep = month[i].amount
+                        } else if (month[i].date == 10) {
+                           Oct = month[i].amount
+                        } else if (month[i].date == 11) {
+                           Nov = month[i].amount
+                        } else if (month[i].date == 12) {
+                           Dec = month[i].amount
+                        }
+                     }
 
-                    monthlyTotal.push(finalData)
-                  }
+                     new Morris.Bar({
+                        element: 'morris-bar-chart',
+                        data: [
+                           { y: 'Jan', a: Jan},
+                           { y: 'Feb', a: Feb},
+                           { y: 'Mar', a: Mar},
+                           { y: 'Apr', a: Apr},
+                           { y: 'May', a: May},
+                           { y: 'Jun', a: Jun},
+                           { y: 'Jul', a: Jul},
+                           { y: 'Aug', a: Aug},
+                           { y: 'Sep', a: Sep},
+                           { y: 'Oct', a: Oct},
+                           { y: 'Nov', a: Nov},
+                           { y: 'Dec', a: Dec}
+                        ],
+                        xkey: 'y',
+                        ykeys: ['a'],
+                        labels: ['Monthly Total'],
+                        barColors: function(row, series, type) {
+                           if (series.key == 'a') {
+                              if (row.y < 0) {
+                                 return "#f12d58";
+                              } else {
+                                 return "#008F70";
+                              }
+                           }
+                        }
+                     })
 
-                }
-
-            })
-            var Jan = Number()
-            var Feb = Number()
-            var Mar = Number()
-            var Apr = Number()
-            var May = Number()
-            var Jun = Number()
-            var Jul = Number()
-            var Aug = Number()
-            var Sep = Number()
-            var Oct = Number()
-            var Nov = Number()
-            var Dec = Number()
-
-            month = monthlyTotal
-
-            for (var i = 0; i <= 12; i++) {
-              if (month[i] === undefined) {
-                i++
-              } else if (month[i].date == 01) {
-                Jan = month[i].amount
-              } else if (month[i].date == 02) {
-                Feb = month[i].amount
-              } else if (month[i].date == 03) {
-                Mar = month[i].amount
-              } else if (month[i].date == 04) {
-                Apr = month[i].amount
-              } else if (month[i].date == 05) {
-                May = month[i].amount
-              } else if (month[i].date == 06) {
-                Jun = month[i].amount
-              } else if (month[i].date == 07) {
-                Jul = month[i].amount
-              } else if (month[i].date == 08) {
-                Aug = month[i].amount
-              } else if (month[i].date == 09) {
-                Sep = month[i].amount
-              } else if (month[i].date == 10) {
-                Oct = month[i].amount
-              } else if (month[i].date == 11) {
-                Nov = month[i].amount
-              } else if (month[i].date == 12) {
-                Dec = month[i].amount
-              }
-            }
-
-            new Morris.Bar({
-                element: 'morris-bar-chart',
-                data: [
-                   { y: 'Jan', a: Jan},
-                   { y: 'Feb', a: Feb},
-                   { y: 'Mar', a: Mar},
-                   { y: 'Apr', a: Apr},
-                   { y: 'May', a: May},
-                   { y: 'Jun', a: Jun},
-                   { y: 'Jul', a: Jul},
-                   { y: 'Aug', a: Aug},
-                   { y: 'Sep', a: Sep},
-                   { y: 'Oct', a: Oct},
-                   { y: 'Nov', a: Nov},
-                   { y: 'Dec', a: Dec}
-                ],
-                xkey: 'y',
-                ykeys: ['a'],
-                labels: ['Monthly Total'],
-                barColors: function(row, series, type) {
-                  if (series.key == 'a') {
-                    if (row.y < 0) {
-                      return "#f12d58";
-                    } else {
-                      return "#008F70";
-                    }
-                  }
-                }
-            })
-
-          }getTotalByMonth(data)
-       }
-    })
-  }call()
+               }reorderList(monthlyTotal)
+            }getTotalByMonth(data)
+         }
+      })
+   }call()
 })
