416e59a975e2aed21ce2986c82452dcb3e7e5756
Marcos Silva <marcosgustavobh@gmail.com>
2017-08-23 23:04:09 +0000

    search bar style

--- public/scripts/tables.js
+++ public/scripts/tables.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
                 }
             }
         }],
-        "dom": 'ftip',
+        "dom": 'tip',
         "order": [[0, "desc"]],
         "iDisplayLength": 200,
         "columns": [
